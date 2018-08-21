8cd1d091c63390e4e1b6f1338e6f72cb4eeaf485
PBOCER <przemek.bocer@gmail.com>
2018-08-21 14:35:43 +0200

    optimistic locking

--- src/main/java/com/capgemini/dao/CustomerDao.java
+++ src/main/java/com/capgemini/dao/CustomerDao.java
@@ -1,8 +1,12 @@
 package com.capgemini.dao;
 
+import com.capgemini.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerDao extends CrudRepository {
+
+
 
 
 }
