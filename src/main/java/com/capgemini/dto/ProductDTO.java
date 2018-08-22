074e5c33759b3b68a22f9a4e38d5522041651085
PBOCER <przemek.bocer@gmail.com>
2018-08-22 15:15:17 +0200

    DTO

---
+++ src/main/java/com/capgemini/dto/ProductDTO.java
@@ -0,0 +1,26 @@
+package com.capgemini.dto;
+
+import com.capgemini.entity.TransactionEntity;
+import com.sun.istack.internal.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDTO {
+
+    private Long id;
+    private String productName;
+    private Long price;
+    private Long margin;
+    private Long weight;
+    private List<Long> transactions;
+
+}
---
+++ src/main/java/com/capgemini/dto/TransactionDTO.java
@@ -0,0 +1,27 @@
+package com.capgemini.dto;
+
+import com.capgemini.entity.ProductEntity;
+import com.capgemini.enums.Status;
+import com.sun.istack.internal.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionDTO {
+
+    private Long id;
+    private Date transactionDate;
+    private Status status;
+    private Long purchasesNumber;
+    private List<Long> products;
+
+}
